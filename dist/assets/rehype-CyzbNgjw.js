599636fed8cf00a345047fd1551e1140ebcf6be7
Dean-anove <dean@anove.ai>
2024-10-24 09:07:47 +0200

    vite.config and workflow file adjustment
    the vite file adjustment downloaded more dist files

---
+++ dist/assets/rehype-CyzbNgjw.js
@@ -0,0 +1,1 @@
+import{u as r}from"./unified-DZpAYcRd.js";import{r as e}from"./rehype-parse-DEOX9LFP.js";import{r as o}from"./rehype-stringify-CYxlCuWR.js";const f=r().use(e).use(o).freeze();export{f as r};
