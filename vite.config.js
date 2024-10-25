f99921533dd646a31f7fdd2b1de2e047edba4485
Dean-anove <dean@anove.ai>
2024-10-25 09:57:53 +0200

    Set base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/release-notes-public/',
   build: {
     rollupOptions: {
       output: {
@@ -19,4 +20,3 @@ export default defineConfig({
     chunkSizeWarningLimit: 1000, // Adjust this based on your needs
   },
 });
-
