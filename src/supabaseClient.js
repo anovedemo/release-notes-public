a252b8ce1419708c0d2929ee2aa98ea417a745d9
ezrabuenkanove <ezra.buenk@anove.agency>
2024-11-15 14:36:00 +0100

    Change supabase config

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -6,8 +6,4 @@ const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 console.log('Supabase URL:', supabaseUrl); // Log URL
 console.log('Supabase Key:', supabaseKey); // Log Key
 
-if (!supabaseUrl || !supabaseKey) {
-    throw new Error('Supabase URL and anon key must be defined');
-}
-
 export const supabase = createClient(supabaseUrl, supabaseKey);
