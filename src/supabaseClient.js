9d7663c98f912b4a32d39f689ed1fd59528a274d
ezrabuenkanove <ezra.buenk@anove.agency>
2024-11-15 14:40:07 +0100

    Change supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,10 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
-<<<<<<< HEAD
-console.log('Supabase URL:', supabaseUrl); // Log URL
-console.log('Supabase Key:', supabaseKey); // Log Key
-
-=======
->>>>>>> jean-hugues
 export const supabase = createClient(supabaseUrl, supabaseKey);
